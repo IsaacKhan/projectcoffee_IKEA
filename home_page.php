6361a166fb4681445fd253cce79ff69180949f25
Isaac Khan <36248421+IsaacKhan@users.noreply.github.com>
2018-05-09 20:12:35 -0500

    asd

--- home_page.php
+++ home_page.php
@@ -140,10 +140,19 @@
 												}
 												else
 												{
-													$sql = "SELECT store_ID, productName
-													FROM product INNER JOIN sales ON product.ID = sales.product_ID
-													WHERE amountSold >0
-													ORDER BY store_ID;";
+													$sql = "SELECT Rank, productName AS Product, units AS 'Units Sold', storeName AS Store
+													FROM (SELECT product_ID AS upc, amountSold AS units_sold, store_ID, storeName,
+															-- Leveraged mysql session variables to track ranking 
+															-- If the store_ID is = to the previous one than increase the rank, otherwise start back at 1
+															  @cur_rank := IF(@store = store_ID, @cur_rank + 1, 1) AS Rank, 
+															  @store := store_ID,
+															  @units := amountSold AS units
+																-- In order to get the correct ranking order a Subquery of each stores products sales in descending order is needed
+																FROM  (SELECT product_ID, amountSold, store_ID, storeName
+																			FROM sales INNER JOIN store on store.ID = sales.store_ID
+																			ORDER BY store_ID, amountSold desc) AS top_items) AS rankings INNER JOIN product ON upc = product.ID
+															-- Cap the ranking at the top 20 items
+															WHERE rank <= 20 ";
 
 													$result = $conn-> query($sql);
 													while($row = $result->fetch_assoc())
